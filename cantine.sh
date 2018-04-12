a479dba51f4d8888cca4ca4e773521985c7ec351
boissonnfive <boissonnfive@gmail.com>
2018-04-12 14:37:54 +0200

    Je ne sais plus ce que j'ai fait.

--- cantine.sh
+++ cantine.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/bash -x
 
 # +---------------------------------------------------------------------------+
 # | Récupère le menu du jour de la cantine de l'école des Dinarelles.         |
@@ -35,35 +35,49 @@
 main()
 {
 	# On récupère le mois en cours
-	MOIS=$(aujourd_hui +%B)                                 # => septembre
-	MOIS=$(premiereLettreEnMajuscule $MOIS)                 # => Septembre
-	# echo $MOIS
+	MOIS_EN_COURS=$(mois_en_cours)
+	MOIS_EN_COURS=$(premiereLettreEnMajuscule $MOIS_EN_COURS)                 # => Septembre
+	# MOIS_EN_COURS="Mars"
+	#echo $MOIS_EN_COURS
 	
 	# On récupère le mois suivant
-	MOIS_SUIVANT=$(aujourd_hui -v+1m +%B)                   # => octobre
-	MOIS_SUIVANT=$(premiereLettreEnMajuscule $MOIS_SUIVANT) # => Octobre
-	# echo $MOIS_SUIVANT
-
+	MOIS_PROCHAIN=$(mois_prochain)
+	MOIS_PROCHAIN=$(premiereLettreEnMajuscule $MOIS_PROCHAIN) # => Octobre
+	# MOIS_PROCHAIN="Avril"
+	#echo $MOIS_PROCHAIN
+	
 	# On récupère la date d'aujourd'hui (format : Jour N°)
-	AUJOURDHUI=$(aujourd_hui "+%A %-d")                     # => Lundi 2, Mardi 15 ou Jeudi 27   
-	# echo $AUJOURDHUI
+	AUJOURDHUI=$(aujourd_hui)
+	# AUJOURDHUI="Vendredi 30"
+	#echo $AUJOURDHUI
 	
 	# On récupère la date de demain (format : Jour N°)
-	DEMAIN=$(aujourd_hui -v+1d "+%A %-d")                    # => Mardi 3, Mercredi 16 ou Vendredi 28
-	# echo $DEMAIN
-	# exit	
+	DEMAIN=$(demain)
+	# DEMAIN="Lundi 2"
+	#echo $DEMAIN	
 	
 	# On récupère le menu de la cantine
 	MENU=$(menu_cantine)
-	# echo "$MENU"
 
 	if [ -z "$MENU" ]; then
-		echo "Il n'y a pas de cantine ce jour : "$(aujourd_hui "+%A %-d %B %Y")
+		echo "Impossible de récupérer le menu à partir de la page web. Vérifiez votre connexion à internet."
 	else
-		echo "Menu du "$(aujourd_hui "+%A %-d %B %Y")" :"
-		echo "$MENU"
-	fi
+		MENU_DU_MOIS=$(menu_du_mois ${MOIS_EN_COURS} ${MOIS_PROCHAIN} "${MENU}")
 
+		if [ -z "$MENU_DU_MOIS" ]; then
+			echo "Impossible de récupérer le menu du mois.\nFin du script."
+		else
+			MENU_DU_JOUR=$(menu_du_jour "${AUJOURDHUI}" "${DEMAIN}" "${MENU_DU_MOIS}")
+
+			if [ -z "$MENU_DU_JOUR" ]; then
+				echo "Il n'y a pas de cantine ce jour : "$(date "+%A %-d %B %Y")
+			else
+				echo "Menu du "$(date "+%A %-d %B %Y")" :"
+				menu_apres_filtre "$MENU_DU_JOUR"
+				# echo "$MENU_DU_JOUR"
+			fi
+		fi
+	fi
 }
 
 # Mets la première lettre du mot en majuscule
@@ -74,13 +88,31 @@ premiereLettreEnMajuscule()
 	echo "$(tr '[:lower:]' '[:upper:]' <<< ${1:0:1})${1:1}" # => Septembre
 }
 
+# Renvoi le mois en cours
+mois_en_cours()
+{
+	date +%B
+}
+
+# Renvoi le mois prochain
+mois_prochain()
+{
+	date -v+1m +%B
+}
+
+
+# Renvoi la date du jour sour la forme : jour N°
+aujourd_hui()
+{
+	date "+%A %-d"
+}
 
 # On récupère la date du prochain jour de cantine
 # Aujourd'hui si on est lundi, mardi, mercredi ou jeudi
 # lundi si on est vendredi/samedi/dimanche
 # $1 : formatage (%B pour le mois, %A %-d pour jour n°)
 # retour : date au format spécifié
-aujourd_hui()
+demain()
 {
 	# formatage date => voir man sfrtime
 	# %A  : le jour de la semaine
@@ -89,14 +121,14 @@ aujourd_hui()
 	# -v(+/-)*  : ajoute ou retranche des unités (d pour jour, m pour mois, y pour année)
 	dec=$(decalage)
 	# echo $@
-	date -v+${dec}d "$@"
+	date -v+${dec}d "+%A %-d"
 }
 
-# On calcule le décalage par rapport à aujourd'hui pour avoir
-# le prochain jour de cantine
+# On calcule le nombre de jours entre aujourd'hui et le prochain
+# jour de cantine
 # Renvoi : le nombre de jours de décalage par rapport à la date du jour
 # Exemples :
-# Vendredi => Lundi : 3 jours
+# Vendredi => Lundi : 3 jours (Si on est vendredi, le prochain jour de cantine est lundi)
 # Samedi   => Lundi : 2 jours
 # Dimanche => Lundi : 1 jours
 # Mercredi => Jeudi : 1 jours
@@ -112,17 +144,19 @@ decalage()
 	elif [ $i == "Samedi" ]; then
 		# demain = lundi = jour + 2
 		RET=2
-	elif [ $i == "Dimanche" -o $i == "Mercredi" ]; then
-		# demain = lundi = jour + 1
-		RET=1
-	else
-		# aujourd'hui
-		RET=0
-	fi
+	# elif [ $i == "Dimanche" -o $i == "Mercredi" ]; then
+	# 	# demain = lundi/jeudi = jour + 1
+	# 	RET=1
+	# else
+	# 	# aujourd'hui
+	# 	RET=0
+	# fi
+    else
+    	RET=1
+    fi
 	
 	echo $RET
 }
-
 
 # Renvoi le menu de la cantine de Rémy
 # Récupéré sur le site de la mairie des Angles
@@ -131,18 +165,56 @@ menu_cantine()
 {
 	curl --silent "http://www.ville-les-angles.fr/fr/menus-des-cantines-1-3-54" | \
 	# On efface les dix dernières lignes du fichier pour ne pas lire des caractères qui ne sont pas en UTF-8
-	sed -e :a -e '$d;N;2,10ba' -e 'P;D' | \
-	# On récupère les menus du mois en cours
-	sed -n "/<h3 > ${MOIS}<\/h3>/,/<h3 > ${MOIS_SUIVANT}<\/h3>/p" | \
-	# On ne garde que ce qu'il y a après <strong>${AUJOURDHUI}<\/strong>
-	#sed -nE "s/.+${AUJOURDHUI}<\/strong>(.+)<strong>.+/\1/p" #| \
-	sed -nE "s/.+${AUJOURDHUI}(.+)${DEMAIN}.+/\1/p" | \
-	#sed -nE "s/.+${AUJOURDHUI}(.+)Vendredi.{2}22.+/\1/p" | \
+	sed -e :a -e '$d;N;2,10ba' -e 'P;D'
+}
+
+
+# Renvoi la liste des menus du mois en cours
+# de la cantine de Rémy
+# $1 : Nom du mois en cours (ex: Septembre)
+# $2 : Nom du mois prochain (ex: Octobre)
+# $3 : Liste complète des menus
+menu_du_mois()
+{
+	# echo ${1}
+	# echo ${2}
+	# echo "${3}"
+	echo "${3}" | sed -n "/<h3 > ${1}<\/h3>/,/<h3 > ${2}<\/h3>/p"
+}
+
+# Renvoi le menu du jour de la cantine de Rémy
+# à partir de la page web du site des angles
+# $1 : Jour d'aujourd'hui (ex: Lundi 2)
+# $2 : Jour suivant       (ex: Mardi 3)
+# $3 : Liste des menus du mois en cours
+menu_du_jour()
+{
+	# echo "${1}"
+	# echo "${2}"
+	# echo "${3}"
+	
+	BORNE_INF=$(echo $AUJOURDHUI | sed -nE "s/[A-Z][a-z]+ ([0-9][0-9]?)/\1/p")
+	BORNE_SUP=$(echo $DEMAIN | sed -nE "s/[A-Z][a-z]+ ([0-9][0-9]?)/\1/p")
+
+	if [ $BORNE_SUP -gt $BORNE_INF ]; then
+		# echo "OK"
+		echo "${3}" | tr -cd '[:print:]\n' | sed -nE "s/.+${1}(.+)${2}.+/\1/p"
+	else
+		# La borne supérieure est inférieure à la borne inférieure => C'est le dernier jour du mois !
+		# echo "${3}" | sed -nE "s/.+${1}(.+)<\/p>.+/\1/p"
+		echo "${3}" | sed -nE "s/.+${1}(.+)/\1/p"
+	fi
+}
+
+
+# Renvoi le menu du jour de la cantine de Rémy
+# à partir de la page web du site des angles
+# $1 : Menu du jour en html
+menu_apres_filtre()
+{
 	# On remplace <br /> par des sauts de ligne
-	sed $'s/<br \/>/\\\n/g' | \
 	# On efface les lignes vides
-	sed '/^$/d'
-	#<strong>[A-Z][a-z]* [0-9]{2}(^[<])*<\/strong>
+	echo "${1}" | sed $'s/<br \/>/\\\n/g' | sed '/^$/d'
 }
 
 
